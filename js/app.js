7a605167ef5205b854c9ac9bc865165deb66c13c
Piotr Morawski <piotrmorawski90@gmail.com>
2019-06-25 21:39:28 +0200

    logic and style refactorings

--- js/app.js
+++ js/app.js
@@ -1,15 +1,30 @@
-var Game = require('./game.js');
+var Game = require("./game.js")
 
+var game = new Game()
 
-var game = new Game();
+game.showFurry()
 
-game.showFurry();
+game.showCoin()
 
-game.showCoin();
+document.addEventListener("keydown", function(event) {
+  game.turnFurry(event)
+})
 
-game.startGame();
+const pauseButton = document.getElementById("pause_button")
+const startButton = document.getElementById("start_button")
 
-document.addEventListener('keydown', function(event){
-        game.turnFurry(event);
-});
+pauseButton.addEventListener("click", () => game.pauseGame())
+startButton.addEventListener("click", () => game.startGame())
 
+const keyDownHandler = e => {
+  switch (e.code) {
+    case "Enter":
+      game.startGame()
+      break;
+    case "Space":
+      game.pauseGame()
+      break;
+  }
+}
+
+window.addEventListener("keydown", keyDownHandler)
